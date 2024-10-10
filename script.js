0bd8001efee160031344345e0a5f1c9e15f0a0d6
Jn-html <marmainjean@gmail.com>
2024-10-10 21:36:34 +0200

    init 'isError' variable

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+// isError used later to check if user provides ivalid input
+let isError = false;
