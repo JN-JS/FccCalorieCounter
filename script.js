f151faaaecdb60df14b5a54548f30bf19cba2982
Jn-html <marmainjean@gmail.com>
2024-10-24 17:49:49 +0200

    query number inputs for dinner, snacks & exercise to calculateCalories()

--- script.js
+++ script.js
@@ -75,6 +75,9 @@ function addEntry() {
       // need to get value from added user's entries
       const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
       const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
+      const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
+      const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+      const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
     }
 
     // Gets the calorie counts from user's entries
